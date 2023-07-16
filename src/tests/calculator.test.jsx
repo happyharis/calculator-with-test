b45146b28e9dfa4d95162b8faa833e1a1fb1397a
Haris Samingan <muhamad_haris@hotmail.com>
2023-07-16 21:59:30 +0800

    add test

--- src/tests/calculator.test.jsx
+++ src/tests/calculator.test.jsx
@@ -27,3 +27,81 @@ test("calculate addition", async () => {
     expect(screen.getByPlaceholderText("Result").value).toBe("12")
   );
 });
+
+// Subtraction test
+test("calculate subtraction", async () => {
+  render(<Calculator />);
+
+  const firstNumInput = screen.getByPlaceholderText("First number");
+  userEvent.type(firstNumInput, "8");
+  await waitFor(() => {
+    expect(firstNumInput.value).toBe("8");
+  });
+
+  const operatorSelect = screen.getByRole("combobox");
+  userEvent.selectOptions(operatorSelect, "-");
+
+  const secondNumInput = screen.getByPlaceholderText("Second number");
+  userEvent.type(secondNumInput, "3");
+  await waitFor(() => {
+    expect(secondNumInput.value).toBe("3");
+  });
+
+  userEvent.click(screen.getByRole("button", { name: "=" }));
+
+  await waitFor(() =>
+    expect(screen.getByPlaceholderText("Result").value).toBe("5")
+  );
+});
+
+// Multiplication test
+test("calculate multiplication", async () => {
+  render(<Calculator />);
+
+  const firstNumInput = screen.getByPlaceholderText("First number");
+  userEvent.type(firstNumInput, "7");
+  await waitFor(() => {
+    expect(firstNumInput.value).toBe("7");
+  });
+
+  const operatorSelect = screen.getByRole("combobox");
+  userEvent.selectOptions(operatorSelect, "*");
+
+  const secondNumInput = screen.getByPlaceholderText("Second number");
+  userEvent.type(secondNumInput, "6");
+  await waitFor(() => {
+    expect(secondNumInput.value).toBe("6");
+  });
+
+  userEvent.click(screen.getByRole("button", { name: "=" }));
+
+  await waitFor(() =>
+    expect(screen.getByPlaceholderText("Result").value).toBe("42")
+  );
+});
+
+// Division test
+test("calculate division", async () => {
+  render(<Calculator />);
+
+  const firstNumInput = screen.getByPlaceholderText("First number");
+  userEvent.type(firstNumInput, "50");
+  await waitFor(() => {
+    expect(firstNumInput.value).toBe("50");
+  });
+
+  const operatorSelect = screen.getByRole("combobox");
+  userEvent.selectOptions(operatorSelect, "/");
+
+  const secondNumInput = screen.getByPlaceholderText("Second number");
+  userEvent.type(secondNumInput, "5");
+  await waitFor(() => {
+    expect(secondNumInput.value).toBe("5");
+  });
+
+  userEvent.click(screen.getByRole("button", { name: "=" }));
+
+  await waitFor(() =>
+    expect(screen.getByPlaceholderText("Result").value).toBe("10")
+  );
+});
